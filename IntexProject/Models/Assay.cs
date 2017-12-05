4ec9c05c78d86aea657fc7b02b569a65b21d50c1
Austin Bird <austin.bird@byu.edu>
2017-12-05 15:34:11 -0700

    connected the database and created an assay model

---
+++ IntexProject/Models/Assay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntexProject.Models
+{
+    public class Assay
+    {
+        [Key]
+        public int assayID { get; set; }
+        public String assayName { get; set; }
+
+
+
+    }
+}
